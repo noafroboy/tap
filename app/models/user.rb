7378f8de3531bce33e9824763c1080aa359da4db
Kevin Liu <kevinliu@Kevin-Lius-MacBook-Pro.local>
2011-01-13 22:46:15 -0800

    only validate presence of on create so i can update attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   acts_as_authentic
-  validates_presence_of :first_name, :last_name, :gender, :email, :password
+  validates_presence_of :first_name, :last_name, :gender, :email, :password, :on => :create
   
 end
