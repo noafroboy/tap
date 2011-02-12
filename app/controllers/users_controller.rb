efcdd6417eff045396de91addf3f34ce08b83a0f
Kevin Liu <kevin.liu@outright.com>
2011-02-12 14:57:59 -0800

    using current_user whenever appropriate cleaned up users_controller. using select tag appropriately in signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,55 +2,38 @@ class UsersController < ApplicationController
   before_filter :require_user, :except => [:new, :create]
   before_filter :require_admin, :only => [:index]
 
-  # GET /users
-  # GET /users.xml
   def index
     @users = User.all
 
     respond_to do |format|
       format.html # index.html.haml
-      format.xml  { render :xml => @users }
     end
   end
 
-  # GET /users/1
-  # GET /users/1.xml
   def show
     @user = User.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.haml
-      format.xml  { render :xml => @user }
     end
   end
 
-  # GET /users/new
-  # GET /users/new.xml
   def new
     @user = User.new
 
     respond_to do |format|
       format.html # new.html.haml
-      format.xml  { render :xml => @user }
     end
   end
 
-  # GET /users/1/edit
   def edit
     @user = User.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.xml
   def create
     user_params = params[:user]
     @user = User.new(user_params)
-    @user.zip_code = user_params["zip_code"] && user_params["zip_code"].to_i
-    @user.years_experience = user_params["years_experience"] && user_params["years_experience"].to_i
-    @user.potential_mentee = user_params["potential_mentee"].downcase.match("yes").nil? ? false : true
-    @user.potential_mentor = user_params["potential_mentor"].downcase.match("yes").nil? ? false : true
-    @user.help_at_workshops_panels = user_params["help_at_workshops_panels"].downcase.match("yes").nil? ? false : true
-    @user.opt_in_directory = user_params["opt_in_directory"].downcase.match("yes").nil? ? false : true
+    @user.zip_code = user_params["zip_code"].nil? ? nil : user_params["zip_code"].to_i
     
     respond_to do |format|
       if @user.save
@@ -63,33 +46,25 @@ class UsersController < ApplicationController
     end
   end
 
-  # PUT /users/1
-  # PUT /users/1.xml
   def update
-    @user = User.find(params[:id])
-
     user_params = params[:user]
     respond_to do |format|
-      if @user.update_attributes(user_params)
-        format.html { redirect_to(@user, :notice => 'User was successfully updated.') }
+      if current_user.update_attributes(user_params)
+        format.html { redirect_to(current_user, :notice => 'User was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.xml
   def destroy
-    puts 'attempting to delete...'
     @user = User.find(params[:id])
-    @user.destroy
-
+    @user.destroy if logged_in_as_admin?
+    
     respond_to do |format|
-      format.html { redirect_to(users_url) }
-      format.xml  { head :ok }
+        format.html { redirect_to(users_url) }
+        format.xml  { head :ok }
     end
   end
 end
