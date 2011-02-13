f55302f625cbbdbf8bd224c581132e5c854e1f86
Kevin Liu <kevin.liu@outright.com>
2011-02-12 17:02:22 -0800

    cleaned up user edit form. it now accurately shows a user's stored information

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ActiveRecord::Base
   acts_as_authentic
   validates_presence_of :first_name, :last_name, :gender, :email, :password, :on => :create
+
+  def has_interest?(interest)
+    (self.areas_of_interest || "").split(',').include?(interest)
+  end
+  
+  def other_interests
+    other_interests = (self.areas_of_interest || "").split(',').reject do |x|
+      ["Community Service", "Cultural", "Dinner Group", "Leadership", "Professional", "Social"].include?(x)
+    end
+    other_interests.join(',')
+  end  
   
 end
--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -3,15 +3,28 @@ $(document).ready(function() {
   $('#new_user').submit(function() {
     var interests = $("input[@class=interest]:checked");
     var user_interests = ""
-    
     $.each(interests, function(index, interest) {
-      user_interests = user_interests + interest['name'] + ",";
       if (interest['id'] == "other") {
         user_interests = user_interests + $(".interest-text")[0]['value']
+      } else {
+        user_interests = user_interests + interest['name'] + ",";
       }
     })
     $('#user_areas_of_interest')[0]["value"] = user_interests
     return true
   });
   
+  $('#user_submit').click(function() {
+    var interests = $("input[@class=interest]:checked");
+    var user_interests = ""
+    $.each(interests, function(index, interest) {
+      if (interest['id'] == "other") {
+        user_interests = user_interests + $(".interest-text")[0]['value']
+      } else {
+        user_interests = user_interests + interest['name'] + ",";
+      }
+    })
+    $('#user_areas_of_interest')[0]["value"] = user_interests
+    return true
+  });
 });
